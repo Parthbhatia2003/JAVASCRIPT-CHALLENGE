a16947ec982079b1de01157b7bbfe1b6a42c3bf8
Parthbhatia2003 <139068349+Parthbhatia2003@users.noreply.github.com>
2024-07-13 10:27:07 +0530

    COMMITED DAY 1

---
+++ DAY1.js
@@ -0,0 +1,42 @@
+//ACTIVITY 1
+var a=18;
+console.log(a);
+
+let b="Parth";
+console.log(b);
+
+//ACTIVITY 2
+const c=true;
+console.log(c);
+
+// ACTIVITY 3
+let q=18;
+let w="Parth";
+let e=true;
+let r={
+    "key":"value",
+    "virat":"kohli",
+}
+let t=[1,2,3,4,5];
+
+console.log(typeof(q));
+console.log(typeof(w));
+console.log(typeof(e));
+console.log(typeof(r));
+console.log(typeof(t));
+
+//ACTIVITY 4
+let m=1;
+console.log(m);
+m=8;
+console.log(m);
+
+//ACTIVITY 5
+const n=1;
+console.log(n);
+n=8;
+console.log(n);
+
+
+
+
