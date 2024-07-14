04a22df01b7116d3590b961960e58acf41d4cc0b
Parthbhatia2003 <139068349+Parthbhatia2003@users.noreply.github.com>
2024-07-14 13:45:23 +0530

    DAY 2 COMMITED

---
+++ DAY2.js
@@ -0,0 +1,53 @@
+// TASK 1
+let a=1;
+let b=8;
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(b/a);
+console.log(b%a);
+
+//ACTIVITY 2
+a+=1;
+console.log(a);
+a-=1;
+console.log(a);
+
+//ACTIVITY 3
+if(a>b){
+    console.log("First number is bigger");
+}
+else{
+    console.log("Second number is bigger");  
+}
+let x=8;
+let y='8';
+
+console.log(x==y);
+console.log(x===y);
+
+//ACTIVITY 4
+let virat=true;
+let kohli=true;
+
+if(virat && kohli){
+    console.log('Yes he is Virat Kohli');  
+}
+if(virat || kohli){
+    console.log('Yes he is Virat or either Kohli');  
+}
+if(!virat){
+    console.log('kohli'); 
+}
+
+//ACTIVITY 5
+let num=18;
+console.log(`Number${num>0?` is positive`:` is negative`}`);
+
+
+
+
+
+
+
+
